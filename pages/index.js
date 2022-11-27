e3175a9fb3f3fc92b91a5ff6e807921b1a3ec155
Edu Ruiz <ruizcantosps@gmail.com>
2022-11-27 21:50:26 +0100

    :)

--- pages/index.js
+++ pages/index.js
@@ -15,10 +15,12 @@ const ContainerHome = styled.div`
 
 const user = "User_" + String(new Date().getTime());
 
+let socket = io(process.env.HOST, {
+  path: '/api/socketio',
+})
+
 const Home = () => {
-  let socket = io(process.env.HOST, {
-    path: '/api/socketio',
-  })
+
 
   const [connected, setConnected] = useState(socket.connected);
   const [chat, setChat] = useState([]);  
